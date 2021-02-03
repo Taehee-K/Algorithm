010fc5df4d7da524495f0051cb3e5b5b8e5f95f9
Taehee-K <taehee107275@gmail.com>
2021-02-03 09:00:36 +0900

    Create 정올 506_출력_자가진단6.py

---
+++ Python/Jungol/출력/정올 506_출력_자가진단6.py
@@ -0,0 +1,7 @@
+# [정올 506/Python] 출력 - 자가진단6
+# http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=143&sca=10
+
+print("My height")
+print(170)
+print("My weight")
+print("{:.6f}".format(68.600000))
