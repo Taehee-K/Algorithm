b8a0b6baa678ae2811e366f5c1d7f6bcb912b368
Taehee-K <taehee107275@gmail.com>
2021-02-05 04:20:33 +0900

    Create 정올 508_출력_자가진단8.py

---
+++ Python/Jungol/출력/정올 508_출력_자가진단8.py
@@ -0,0 +1,7 @@
+# [정올 508/Python] 출력 - 자가진단8
+# http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=145&sca=10
+
+print("{:>10}{:>10}{:>10}".format('item', 'count', 'price'))
+print("{:>10}{:>10}{:>10}".format('pen', 20, 100))
+print("{:>10}{:>10}{:>10}".format('note', 5, 95))
+print("{:>10}{:>10}{:>10}".format("eraser", 110, 97))
