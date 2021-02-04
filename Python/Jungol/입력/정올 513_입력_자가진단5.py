07ad02d2a2dd1e7770f227dda3790c1ba2710dab
Taehee-K <taehee107275@gmail.com>
2021-02-05 05:23:32 +0900

    입력

---
+++ Python/Jungol/입력/정올 513_입력_자가진단5.py
@@ -0,0 +1,11 @@
+# [정올 513/Python] 입력 - 자가진단5
+# http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=150&sca=1020
+
+yard_cm = 91.44
+inch_cm = 2.54
+
+yard = 2.1
+inch = 10.5
+
+print("{:>4.1f}yd = {:>5.1f}cm".format(yard, yard_cm*yard))
+print("{:>4.1f}in = {:>5.1f}cm".format(inch, inch_cm*inch))
---
+++ Python/Jungol/입력/정올 514_입력_자가진단6.py
@@ -0,0 +1,5 @@
+# [정올 514/Python] 입력 - 자가진단6
+# http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=151&sca=1020
+
+height = int(input("height = "))
+print("Your height is "+str(height)+'cm.')
