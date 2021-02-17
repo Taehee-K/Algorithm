9e56955c91f0ab5dfd3b22048ba107ef1b446903
Taehee-K <taehee107275@gmail.com>
2021-02-17 23:10:35 +0900

    Update 정올 523_연산자_자가진단6.py

--- Python/Jungol/연산자/정올 523_연산자_자가진단6.py
+++ Python/Jungol/연산자/정올 523_연산자_자가진단6.py
@@ -1,23 +1,23 @@
 # [정올 523/Python] 연산자 - 자가진단6
 # http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=160&sca=1030
-
+ 
 x, y = map(int, input().split())
-
+ 
 if x>y:
     print(x,'>',y,'---',1)
 else:
     print(x,'>',y,'---',0)
-
+ 
 if x<y:
     print(x,'<',y,'---',1)
 else:
     print(x,'<',y,'---',0)
-
+ 
 if x>=y:
     print(x,'>=',y,'---',1)
 else:
     print(x,'>=',y,'---',0)
-
+ 
 if x<=y:
     print(x,'<=',y,'---',1)
 else:
