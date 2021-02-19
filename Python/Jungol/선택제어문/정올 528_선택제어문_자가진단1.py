00555031bb516b31d93c15c8b8fdc3a1508d8cc3
Taehee-K <taehee107275@gmail.com>
2021-02-19 22:29:37 +0900

    Create 정올 528_선택제어문_자가진단1.py

---
+++ Python/Jungol/선택제어문/정올 528_선택제어문_자가진단1.py
@@ -0,0 +1,11 @@
+# [정올 528/Python] 선택제어문 - 자가진단1
+# http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=165&sca=1050
+ 
+# 정수를 입력
+num = int(input())
+
+# 첫 줄에 입력 받은 숫자를 출력
+print(num)
+
+# 둘째 줄에 음수이면 “minus” 라고 출력
+if num<0: print('minus')
