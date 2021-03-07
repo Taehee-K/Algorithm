7b42b8130d2135a26c17751f9919d9a099df96aa
Taehee-K <taehee107275@gmail.com>
2021-03-07 23:53:01 +0900

    Create 정올 588_함수3_자가진단2.py

---
+++ Python/Jungol/함수3/정올 588_함수3_자가진단2.py
@@ -0,0 +1,10 @@
+# [정올 588/Python] 함수3 - 자가진단2
+# http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=225&sca=10d0
+
+def Recursive(n):
+    if n>0:
+        print('recursive')
+        Recursive(n-1)
+
+n = int(input())
+Recursive(n)
