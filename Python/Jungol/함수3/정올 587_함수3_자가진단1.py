b6322d11771079d7620ecd6335d0111080b5b548
Taehee-K <taehee107275@gmail.com>
2021-03-08 21:57:44 +0900

    Update 정올 587_함수3_자가진단1.py

--- Python/Jungol/함수3/정올 587_함수3_자가진단1.py
+++ Python/Jungol/함수3/정올 587_함수3_자가진단1.py
@@ -1,9 +1,10 @@
 # [정올 587/Python] 함수3 - 자가진단1
 # http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=224&sca=10d0
-
+ 
 def Recursive(n):
-    for i in range(n):
-        print(n-i, end = " ")
-
+    if n>0:
+        print('recursive')
+        Recursive(n-1)
+ 
 n = int(input())
 Recursive(n)
