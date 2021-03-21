94f6093c5b06a0e8826a33eec640088d47282f95
Taehee-K <taehee107275@gmail.com>
2021-03-21 21:44:18 +0900

    Create 정올 233_함수3_형성평가3.py

---
+++ Python/Jungol/함수3/정올 233_함수3_형성평가3.py
@@ -0,0 +1,9 @@
+# [정올 233/Python] 함수3 - 형성평가3
+# http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=133&sca=10d0
+
+def Dice(N, M):
+
+    Dice(N+1, M+i)
+N, M = map(int, input().split())
+arr = []
+Dice(N, M)
