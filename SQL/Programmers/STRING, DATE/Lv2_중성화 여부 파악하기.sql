4693e82e62422719899afb76880aab2ec896e285
Taehee-K <taehee107275@gmail.com>
2021-04-07 18:50:00 +0900

    string, date

---
+++ SQL/Programmers/STRING, DATE/Lv2_중성화 여부 파악하기.sql
@@ -0,0 +1,10 @@
+-- [프로그래머스 Lv2/SQL] 중성화 여부 파악하기
+-- https://programmers.co.kr/learn/courses/30/parts/17047
+
+SELECT ANIMAL_ID, NAME, 
+    CASE 
+    WHEN SEX_UPON_INTAKE IN ('Neutered Male', 'Spayed Female') THEN 'O'
+    ELSE 'X'
+    END AS '중성화'    -- END 지정해 주어야 함
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
