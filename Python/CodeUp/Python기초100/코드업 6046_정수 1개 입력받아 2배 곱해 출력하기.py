da6e97e7a0e2cb706edef67115528d0d9b3543fd
Taehee-K <taehee107275@gmail.com>
2021-04-11 04:06:04 +0900

    비트시프트연산

---
+++ Python/CodeUp/Python기초100/코드업 6046_정수 1개 입력받아 2배 곱해 출력하기.py
@@ -0,0 +1,8 @@
+# [코드업 6046/Python] 비트시프트연산 - 정수 1개 입력받아 2배 곱해 출력하기
+# https://codeup.kr/problem.php?id=6046
+
+num = int(input())
+# << : 2배 곱하기
+# >> : 2로 나누기
+print(num<<1)
+
---
+++ Python/CodeUp/Python기초100/코드업 6047_2의 거듭제곱 배로 곱해 출력하기.py
@@ -0,0 +1,8 @@
+# [코드업 6047/Python] 비트시프트연산 - 2의 거듭제곱 배로 곱해 출력하기
+# https://codeup.kr/problem.php?id=6047
+
+a, b = map(int, input().split())
+# << : 2배 곱하기
+# >> : 2로 나누기
+print(a<<b)
+
