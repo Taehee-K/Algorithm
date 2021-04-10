1d098fe8b8402e749b3fdc8f1f25ce6bc6f31012
Taehee-K <taehee107275@gmail.com>
2021-04-11 03:02:53 +0900

    출력변환

--- Python/CodeUp/Python기초100/코드업 6027_10진 정수 입력받아 16진수로 출력하기1.py
+++ Python/CodeUp/Python기초100/코드업 6027_10진 정수 입력받아 16진수로 출력하기1.py
@@ -1,4 +1,4 @@
-# [코드업 6027/Python] 값변환 - 10진 정수 입력받아 16진수로 출력하기1
+# [코드업 6027/Python] 출력변환 - 10진 정수 입력받아 16진수로 출력하기1
 # https://codeup.kr/problem.php?id=6027
 
 a = int(input())
--- Python/CodeUp/Python기초100/코드업 6028_10진 정수 입력받아 16진수로 출력하기2.py
+++ Python/CodeUp/Python기초100/코드업 6028_10진 정수 입력받아 16진수로 출력하기2.py
@@ -1,4 +1,4 @@
-# [코드업 6028/Python] 값변환 - 10진 정수 입력받아 16진수로 출력하기2
+# [코드업 6028/Python] 출력변환 - 10진 정수 입력받아 16진수로 출력하기2
 # https://codeup.kr/problem.php?id=6028
 
 a = int(input())
