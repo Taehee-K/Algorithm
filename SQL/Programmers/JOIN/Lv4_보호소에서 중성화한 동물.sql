ca59efc59f9cc7a5e00b07fe66bf5b35edef1035
Taehee-K <taehee107275@gmail.com>
2021-04-22 17:00:35 +0900

    join

---
+++ SQL/Programmers/JOIN/Lv4_보호소에서 중성화한 동물.sql
@@ -0,0 +1,11 @@
+-- [프로그래머스 Lv4/SQL] 보호소에서 중성화한 동물
+-- https://programmers.co.kr/learn/courses/30/lessons/59045
+
+SELECT INS.ANIMAL_ID, INS.ANIMAL_TYPE, INS.NAME
+FROM ANIMAL_INS INS
+    INNER JOIN ANIMAL_OUTS OUTS
+    ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE SEX_UPON_INTAKE LIKE 'Intact%' AND 
+    (SEX_UPON_OUTCOME LIKE 'Spayed%' OR
+     SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY INS.ANIMAL_ID
