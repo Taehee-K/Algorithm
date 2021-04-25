88fccd3f2108643836d9e422b1f183de5fd002d8
Taehee-K <taehee107275@gmail.com>
2021-04-25 21:53:17 +0900

    string, date

--- SQL/Programmers/STRING, DATE/Lv2_중성화 여부 파악하기.sql
+++ SQL/Programmers/STRING, DATE/Lv2_중성화 여부 파악하기.sql
@@ -1,5 +1,5 @@
 -- [프로그래머스 Lv2/SQL] 중성화 여부 파악하기
--- https://programmers.co.kr/learn/courses/30/parts/17047
+-- https://programmers.co.kr/learn/courses/30/lessons/59409
 
 SELECT ANIMAL_ID, NAME, 
     CASE 
