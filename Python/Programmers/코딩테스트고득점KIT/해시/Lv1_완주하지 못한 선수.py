bc23e30469089070da69830581f9f96fafd3ab5b
Taehee-K <taehee107275@gmail.com>
2021-05-04 21:30:38 +0900

    python-programmers

---
+++ Python/Programmers/코딩테스트고득점KIT/해시/Lv1_완주하지 못한 선수.py
@@ -0,0 +1,28 @@
+# [프로그래머스 Lv1/Python] 정렬 - 완주하지 못한 선수
+# https://programmers.co.kr/learn/courses/30/lessons/42576
+
+from collections import Counter
+def solution(participant, completion):
+    p = Counter(participant)
+    c = Counter(completion)
+    if set(participant) == set(completion): # 동명이인이 있을 때
+        for name in list(p.keys()):
+            if p[name] != c[name]: return name
+    else:                                   # 동명이인이 없을 때
+        for name in list(p.keys()):
+            if name not in c.keys():
+                return name
+
+
+# 시간초과 
+def solution(participant, completion):
+    p = Counter(participant)
+    c = Counter(completion)
+    if set(participant) == set(completion): # 동명이인이 있을 때
+        for name in list(p.keys()):
+            if p[name] != c[name]: return name
+    else:   # 동명이인이 없을 때
+            # 여기서의 for 문 때문에 시간초과 생긴 것으로 보임
+        name = [name for name in participant if name not in completion] 
+        return name[0]
+        
