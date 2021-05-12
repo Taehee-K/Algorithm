541e654e2b0006606fd3cc364e4bd93636d0414e
Taehee-K <taehee107275@gmail.com>
2021-05-12 22:41:00 +0900

    hackerrank basic select

---
+++ SQL/HackerRank/Lv1_Revising the Select Query2.sql
@@ -0,0 +1,6 @@
+-- [HackerRank Easy/SQL] Revising the Select Query II
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
+SELECT NAME 
+FROM CITY 
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000
