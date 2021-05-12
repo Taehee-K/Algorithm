f77f424ac7e661680e9dac236c66ec9e99c6d6d4
Taehee-K <taehee107275@gmail.com>
2021-05-12 22:50:32 +0900

    sql-hackerrank

---
+++ SQL/HackerRank/Basic Select/Easy_Select All.sql
@@ -0,0 +1,4 @@
+-- [HackerRank Easy/SQL] Select All
+-- https://www.hackerrank.com/challenges/select-all-sql/problem
+
+SELECT* FROM CITY
---
+++ SQL/HackerRank/Basic Select/Easy_Select By ID.sql
@@ -0,0 +1,5 @@
+-- [HackerRank Easy/SQL] Select By ID
+-- https://www.hackerrank.com/challenges/select-by-id/problem
+
+SELECT * FROM CITY 
+WHERE ID = 1661
