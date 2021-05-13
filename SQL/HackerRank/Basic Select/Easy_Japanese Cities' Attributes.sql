186672f683b1aeade3e9dc3c1fed330945abe246
Taehee-K <taehee107275@gmail.com>
2021-05-13 23:40:45 +0900

    sql-hackerrank

---
+++ SQL/HackerRank/Basic Select/Easy_Japanese Cities' Attributes.sql
@@ -0,0 +1,6 @@
+-- [HackerRank Easy/SQL] Japanese Cities' Attributes
+-- https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+SELECT * 
+FROM CITY 
+WHERE COUNTRYCODE = 'JPN'
---
+++ SQL/HackerRank/Basic Select/Easy_Japanese Cities' Names.sql
@@ -0,0 +1,6 @@
+-- [HackerRank Easy/SQL] Japanese Cities' Names
+-- https://www.hackerrank.com/challenges/japanese-cities-name/problem
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
