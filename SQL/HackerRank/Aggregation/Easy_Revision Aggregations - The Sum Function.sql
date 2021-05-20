c4068ea5511217ea497e7df3e67869e2bd9737ef
Taehee-K <taehee107275@gmail.com>
2021-05-20 23:50:04 +0900

    sql-hackerrank

---
+++ SQL/HackerRank/Aggregation/Easy_Revision Aggregations - The Sum Function.sql
@@ -0,0 +1,6 @@
+-- [HackerRank Easy/SQL] Revising Aggregations - The Sum Function
+-- https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California'
