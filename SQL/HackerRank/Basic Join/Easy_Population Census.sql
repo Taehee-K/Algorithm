85fcbebad5e9c7ea89ee7a949da3af7aa36d4d9b
Taehee-K <taehee107275@gmail.com>
2021-06-07 06:15:08 +0900

    sql-hackerrank

---
+++ SQL/HackerRank/Basic Join/Easy_Population Census.sql
@@ -0,0 +1,6 @@
+-- [HackerRank Easy/SQL] Population Census
+-- https://www.hackerrank.com/challenges/asian-population/problem
+
+SELECT SUM(CITY.POPULATION)
+FROM CITY, COUNTRY
+WHERE CITY.COUNTRYCODE = COUNTRY.CODE AND COUNTRY.CONTINENT = 'Asia'
