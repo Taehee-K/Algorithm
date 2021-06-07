bc91d641713c9ad5af6e8e68f39a7a56c456e3b2
Taehee-K <taehee107275@gmail.com>
2021-06-08 00:23:03 +0900

    sql-hackerrank

---
+++ SQL/HackerRank/Basic Join/Easy_African Cities.sql
@@ -0,0 +1,6 @@
+-- [HackerRank Easy/SQL] African Cities
+-- https://www.hackerrank.com/challenges/african-cities/problem
+
+SELECT CITY.NAME
+FROM CITY, COUNTRY
+WHERE CITY.COUNTRYCODE = COUNTRY.CODE AND COUNTRY.CONTINENT = 'Africa'
